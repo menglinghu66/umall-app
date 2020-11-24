28a5e7614d1beca32fe349bef812ddaff044a2ef
123 <11@qq.com>
2020-11-24 19:31:57 +0800

    umall-app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//assets
+import  "./assets/css/assets.css"
+import "./assets/js/rem"
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
+
